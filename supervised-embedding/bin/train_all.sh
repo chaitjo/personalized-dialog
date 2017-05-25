5b631910dfeabd66d34f36a6876356e257fd3e5d
root <root@iccluster046.iccluster.epfl.ch>
2017-05-25 10:07:24 +0200

    test

--- supervised-embedding/bin/train_all.sh
+++ supervised-embedding/bin/train_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-bin/task1.sh True > log/task1
-bin/task2.sh True > log/task2
+#bin/task1.sh True > log/task1
+#bin/task2.sh True > log/task2
 bin/task3.sh True > log/task3
 bin/task4.sh True > log/task4
 bin/task5.sh True > log/task5
